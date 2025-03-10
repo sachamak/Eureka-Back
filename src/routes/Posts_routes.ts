21752b009540ac34db6690a988430a856117e198
sachamak <sachachamak@gmail.com>
2025-03-10 13:44:13 +0200

    a

--- src/routes/Posts_routes.ts
+++ src/routes/Posts_routes.ts
@@ -6,7 +6,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/");
+    cb(null, "public/"); 
   },
   filename: function (req, file, cb) {
     const ext = file.originalname
