65b6591896c665a87dca2dda5fce6771743d3bd2
sachamak <sachachamak@gmail.com>
2025-03-12 18:31:12 +0200

    added func update and deletebyid in comment controller

--- src/controllers/comments_controller.ts
+++ src/controllers/comments_controller.ts
@@ -1,7 +1,10 @@
+/** @format */
+
 import commentsModel, { iComment } from "../models/comments_model";
 import BaseController from "./base_controller";
 import { Request, Response } from "express";
 import PostModel from "../models/posts_model";
+import mongoose from "mongoose";
 
 class commentsController extends BaseController<iComment> {
   constructor() {
@@ -27,6 +30,60 @@ class commentsController extends BaseController<iComment> {
     } catch (error) {
       console.error("Error creating comment:", error);
       res.status(500).send({ message: "Server error", error });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      const userId = req.params.userId;
+      const comment = { ...req.body, owner: userId };
+      const updatedComment = await this.model.findByIdAndUpdate(
+        req.params.commentId,
+        comment,
+        { new: true }
+      );
+      if (!updatedComment) {
+        return res.status(404).send( "Comment not found");
+      }
+      res.status(200).send(updatedComment);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+
+  async deleteById(req: Request, res: Response): Promise<void> {
+    try {
+      const id = req.params.id;
+
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).send("Invalid comment ID format" );
+        return;
+      }
+
+      const commentToDelete = await this.model.findById(id);
+      if (!commentToDelete) {
+        res.status(404).send("Comment not found");
+        return;
+      }
+
+      await this.model.findByIdAndDelete(id);
+
+      const postId = req.body.postId || commentToDelete.postId;
+      if (postId && mongoose.Types.ObjectId.isValid(postId)) {
+        await PostModel.findByIdAndUpdate(
+          postId,
+          {
+            $pull: {
+              comments: id,
+            },
+          },
+          { new: true }
+        );
+      }
+
+      res.status(200).send( "Comment deleted successfully" );
+    } catch (error) {
+      res.status(500).send(error);
     }
   }
 }
